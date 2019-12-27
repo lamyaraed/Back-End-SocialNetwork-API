dfb6bc49be8e512ed16b7bbd60781a80641c92cd
fatmaashraframadan <36447147+fatmaashraframadan@users.noreply.github.com>
2019-12-28 01:03:10 +0200

    edited

--- Back-End-SocailMedia-API/src/com/company/AccountManagement/AccountTest.java
+++ Back-End-SocailMedia-API/src/com/company/AccountManagement/AccountTest.java
@@ -1,5 +1,6 @@
 package com.company.AccountManagement;
 
+import com.company.UserProfile.User;
 import com.company.UserProfile.UserDB;
 
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,13 @@ class AccountTest {
 
     @Test
     void logIn() {
-        String username = "Mariam";
-        String Password = "mariam123";
+        String username = "Fatma";
+        String Password = "fatma264541";
         Account test = new Account();
-       String s=  test.LogIn(username,Password);
-       UserDB ob = new UserDB();
-        assertEquals("Done!!",s);
+   //     User ob = new User("Fatma", "Ashraf", "FatmaAshraf", "Fatma.ashrafgmail.com", "fatma264541",16, "Egypt", "Female", "Regular");
 
+        String s=  test.LogIn(username,Password);
+        assertEquals("Done!!",s);
     }
 
     @Test
